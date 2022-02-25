6ed75ca4ff98dbe8666d6affb008493b94f46c03
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-02-25 20:39:51 +0900

    [add] GitHubRepositoriesRepository

---
+++ iOSEngineerCodeCheck/Common/Repository/GitHubRepositoriesRepository.swift
@@ -0,0 +1,28 @@
+//
+//  GitHubRepositoriesRepository.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Tomoya Tanaka on 2022/02/22.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+import Moya
+import RxSwift
+
+protocol GitHubRepositoriesRepositoryType {
+    func fetchRepositories(searchQuery: String) -> Single<GitHubRepositories>
+}
+
+final class GitHubRepositoriesRepository: GitHubRepositoriesRepositoryType {
+    let apiClient: GitHubAPIClientType
+
+    init(apiClient: GitHubAPIClientType) {
+        self.apiClient = apiClient
+    }
+
+    func fetchRepositories(searchQuery: String) -> Single<GitHubRepositories> {
+        let targetType = SearchRepositoriesRequest(searchQuery: searchQuery)
+        return apiClient.send(targetType: targetType, responseType: GitHubRepositories.self)
+    }
+}
