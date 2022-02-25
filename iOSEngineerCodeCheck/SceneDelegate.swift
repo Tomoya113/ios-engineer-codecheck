9b3365fa76ca2766cbd5496f16e3591e0f8cb1e1
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-02-26 04:48:14 +0900

    [update] SceneDelegate

--- iOSEngineerCodeCheck/SceneDelegate.swift
+++ iOSEngineerCodeCheck/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 YUMEMI Inc. All rights reserved.
 //
 
+import Moya
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -14,10 +15,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let apiClient = GitHubAPIClient(provider: MoyaProvider<MultiTarget>())
+        let gitHubRepositoriesRepository = GitHubRepositoriesRepository(apiClient: apiClient)
+        let model = SearchRepositoriesModel(gitHubRepositoriesRepository: gitHubRepositoriesRepository)
+        let viewModel = SearchRepositoriesViewModel(model: model)
+        let router = SearchRepositoriesRouter()
+        let searchRepositoriesViewController = UIStoryboard(name: "Main", bundle: .main)
+            .instantiateViewController(identifier: "SearchRepositories") { coder in
+                return SearchRepositoriesViewController(coder: coder, viewModel: viewModel, router: router)
+            }
+        let initialViewController = UINavigationController(rootViewController: searchRepositoriesViewController)
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
